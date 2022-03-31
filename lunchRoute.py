5d16e15e7149c95c22d516c48a58ad94f8e36900
Michael Wu <michaelwu1002@gmail.com>
2022-03-31 16:34:06 -0700

    added pseudocode and algorithm plans

--- lunchRoute.py
+++ lunchRoute.py
@@ -1,2 +1,22 @@
+def assignLocations(drivers, locations):
+    # naive alg:
+    # 1. assign a random location to each of the drivers
+    # 2. loop through each driver (random order again) and add the location that is nearest to the driver
+    pass
+
+def findOptimalRoute(driversAndLocation):
+    # traveling salesman problem
+    pass
+
+def calculateCost(driversAndLocation):
+    # calculate the time it takes each driver to go to their locations based on the optimal route
+    # if any cost exceeds the time required to deliver all food, rerun assignLocations (the entire alg)
+    # if after 10 tries, there still isn't an optimal route, then we will manually make edits to the best plan the alg gives us
+    pass
+
+
+
 if __name__ == "__main__":
+    # must import number of drivers and a list of all the locations
+    # assignLocations(drivers, locations)
     pass
